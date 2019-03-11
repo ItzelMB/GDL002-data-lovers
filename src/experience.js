0710cc2138fe4a3b8a48404c929bec4d8a66f518
ItzelMB <it.mobec@gmail.com>
2019-03-11 17:01:22 -0600

    Estructura HTML completa en experience.html, links de botones para flujo de pantallas y conexión con index.html funcionales

--- src/experience.js
+++ src/experience.js
@@ -1,1 +1,14 @@
-const goToPokedex = document.querySelector('#intro');
+//const goToPokedex = document.querySelector('#intro');
+
+const showQuizzScreen = () => {
+    intro.style.display = 'none';
+    discover.style.display = 'block';
+};
+document.getElementById('discoverPoke').addEventListener('click', showQuizzScreen);
+
+const showResultSreen = () => {
+    intro.style.display = 'none';
+    discover.style.display = 'none';
+    resultsPoke.style.display = 'block';
+};
+document.getElementById('discoverResults').addEventListener('click', showResultSreen);
